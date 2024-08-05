db547ecf430d7018dcc7427b9c2a4e85399640e4
Zi Chen (from Dev Box) <zijchen@microsoft.com>
2024-08-05 13:59:33 -0700

    Add PackageReference test

--- test/Microsoft.Build.Sql.Tests/CodeAnalysisTests.cs
+++ test/Microsoft.Build.Sql.Tests/CodeAnalysisTests.cs
@@ -15,6 +15,8 @@ namespace Microsoft.Build.Sql.Tests
         {
             string tempFolder = Path.Combine(Path.GetTempPath(), TestContext.CurrentContext.Test.Name);
             TestUtils.CopyDirectoryRecursive(Path.Combine(this.CommonTestDataDirectory, "CodeAnalyzerSample"), tempFolder);
+
+            // Add the analyzer csproj as a ProjectReference to the test sqlproj
             ProjectUtils.AddItemGroup(this.GetProjectFilePath(), "ProjectReference",
                 new string[] { Path.Combine(tempFolder, "CodeAnalyzerSample.csproj") },
                 item =>
@@ -25,6 +27,41 @@ namespace Microsoft.Build.Sql.Tests
                     item.AddMetadata("SetTargetFramework", "TargetFramework=netstandard2.1");
                 });
 
+            // Set up code analysis properties
+            ProjectUtils.AddProperties(this.GetProjectFilePath(), new Dictionary<string, string>()
+            {
+                { "RunSqlCodeAnalysis", "true" },
+                { "SqlCodeAnalysisRules", "+!CodeAnalyzerSample.TableNameRule001" }   // Should fail build on this rule
+            });
+
+            int exitCode = this.RunDotnetCommandOnProject("build", out string stdOutput, out string stdError);
+
+            Assert.AreNotEqual(0, exitCode, "Build should have failed");
+            Assert.IsTrue(stdOutput.Contains("Table name [dbo].[NotAView] ends in View. This can cause confusion and should be avoided"), "Unexpected stderr");
+        }
+
+        [Test]
+        public void VerifyCodeAnalyzerFromPackageReference()
+        {
+            // Set up and create the analyzer package
+            string tempFolder = Path.Combine(Path.GetTempPath(), TestContext.CurrentContext.Test.Name);
+            TestUtils.CopyDirectoryRecursive(Path.Combine(CommonTestDataDirectory, "CodeAnalyzerSample"), tempFolder);
+            RunGenericDotnetCommand($"pack {Path.Combine(tempFolder, "CodeAnalyzerSample.csproj")} -o {tempFolder}", out _, out _);
+
+            // Copy analyzer package to local Nuget source
+            string analyzerPackagePath = Path.Combine(tempFolder, "CodeAnalyzerSample.1.0.0.nupkg");
+            FileAssert.Exists(analyzerPackagePath);
+            File.Copy(analyzerPackagePath, Path.Combine(WorkingDirectory, "pkg", "CodeAnalyzerSample.1.0.0.nupkg"));
+
+            // Add the analyzer package as a PackageReference to the test sqlproj
+            ProjectUtils.AddItemGroup(this.GetProjectFilePath(), "PackageReference",
+                new string[] { "CodeAnalyzerSample" },
+                item =>
+                {
+                    item.AddMetadata("Version", "1.0.0");
+                });
+
+            // Set up code analysis properties
             ProjectUtils.AddProperties(this.GetProjectFilePath(), new Dictionary<string, string>()
             {
                 { "RunSqlCodeAnalysis", "true" },
